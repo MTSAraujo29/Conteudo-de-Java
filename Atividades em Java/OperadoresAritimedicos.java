7ac9ffabe842163199874a4b2a0047db3af9cad7
Matheus Ferreira De Araujo <matheusferreiradearaujo19@gmail.com>
2023-03-06 17:04:49 -0300

    mais uma

---
+++ Atividades em Java/OperadoresAritimedicos.java
@@ -0,0 +1,9 @@
+public class OperadoresAritimedicos {
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float re = (n1 + n2)/2;
+        System.out.println("O resultado é: " + re);
+    }
+}
